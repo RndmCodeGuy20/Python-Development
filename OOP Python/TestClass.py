d5d8e8a955347137474cedc5f2ac422f8ce0c702
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-21 16:14:11 +0530

    code UPR, cheat sheet added : master

--- OOP Python/TestClass.py
+++ OOP Python/TestClass.py
@@ -1,9 +1,11 @@
 class Student:
-    def __init__(self, name, age):
+    def __init__(self, name, age, std):
         self.name = name
         self.age = age
+        self.std = std
 
-S1 = Student("Shantanu", 19)
+S1 = Student("Shantanu", 19, 13)
 
 print(S1.name)
 print(S1.age)
+print(S1.std)
