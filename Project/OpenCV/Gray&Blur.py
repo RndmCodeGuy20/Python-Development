84ba75ee53c1de13498722e4427df40cfefa9f2f
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-02 12:59:33 +0530

    grayscale,blur and canny UPR : master

---
+++ Project/OpenCV/Gray&Blur.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread("D:\Python Development\Resource\Images\1138974.jpg")
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #* Image Gray
+imgBlur = cv2.GaussianBlur(img,(7,7),0) #* Image Blur
+imgCanny = cv2.Canny(img, 100,100)
+
+cv2.imshow("Gray Image Output",imgGray)
+cv2.imshow("Canny Image Output",imgCanny)
+cv2.imshow("Blur Image Output",imgBlur)
+
+cv2.waitKey(20000)
