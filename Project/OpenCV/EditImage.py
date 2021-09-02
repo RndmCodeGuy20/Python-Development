9bf8def203fad8b8a481a297c68b1a4a883dc438
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-02 14:07:14 +0530

    rectangle added : master

--- Project/OpenCV/EditImage.py
+++ Project/OpenCV/EditImage.py
@@ -5,7 +5,8 @@ img = np.zeros((360,360,3), np.uint8)
 # print(img.shape)
 # img[:] = 250,255,157
 
-cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3) #* 3 is for thickness
+cv2.rectangle(img,(0,0),(250,300),(0,0,255),3)
 
 cv2.imshow("Output", img)
 
