0df929ac3ba068b90e6bb7f57f69326c40b49e97
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-02 14:04:23 +0530

    edit and create a vector image : master

---
+++ Project/OpenCV/EditImage.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = np.zeros((360,360,3), np.uint8)
+# print(img.shape)
+# img[:] = 250,255,157
+
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+
+cv2.imshow("Output", img)
+
+cv2.waitKey(5000)
