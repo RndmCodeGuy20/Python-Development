f890c8dcb94a016fdb02cace210dfa017e6e16be
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-02 12:45:41 +0530

    dir change and Cam Read UPR : master

--- Project/OpenCV/CamRead.py
+++ Project/OpenCV/CamRead.py
@@ -1,6 +1,9 @@
 import cv2
 
-cap = cv2.VideoCapture("D:\Python Development\Resource\Images\crowd-of-people-walking-815307-PREVIEW.mp4")
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100) #* Set Brightness
 
 while True:
     success, img = cap.read()
