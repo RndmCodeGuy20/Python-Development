f58ee12595874fdc9621c30d62c7d117037567c3
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-21 20:20:01 +0530

    new file : master

---
+++ Project/OpenCV/IdenCol.py
@@ -0,0 +1,11 @@
+import cv2
+
+path = "D:\Python Development\Resource\Images\siyuan-hDzllryPB9g-unsplash.jpg"
+img = cv2.imread(path)
+
+imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+cv2.imshow("Original", img)
+cv2.imshow("HSV Output", imgHSV)
+
+cv2.waitKey(5000)
