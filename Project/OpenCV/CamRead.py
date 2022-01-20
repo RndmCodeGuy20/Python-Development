c75f04b4bee6d46e8eb320ef1821c692ac0f0e41
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-01-20 19:26:29 +0530

    some codes : master

--- Project/OpenCV/CamRead.py
+++ Project/OpenCV/CamRead.py
@@ -3,7 +3,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
-cap.set(10, 100)  # * Set Brightness
+cap.set(10, 100)  #* Set Brightness
 
 while True:
     success, img = cap.read()
---
+++ Project/Turtle Codes 🐢/FirstProgram.py
@@ -0,0 +1,10 @@
+import turtle as sam
+
+sam.pencolor("red")
+sam.fillcolor("cyan")
+sam.begin_fill()
+
+for i in range(0, 10):
+    sam.forward(100)
+    sam.left(40)
+sam.end_fill()
